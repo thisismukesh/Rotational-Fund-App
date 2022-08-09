ea9685d811a6f83d431c7162073fc1af3c480d1a
Ananmay <ananmays20@gmail.com>
2022-08-08 18:59:13 -0700

    Initial Commit

---
+++ Shared/FundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundAppApp.swift
+//  Shared
+//
+//  Created by Ananmay Singh on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
